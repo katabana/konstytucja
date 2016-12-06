166d0886316baa1e6816c07bcb34c5c52dd2c960
katabana <kokanuga@gmail.com>
2016-12-06 23:52:24 +0100

    beginning of implementation

--- src/konstytucja/KonstytucjaParser.java
+++ src/konstytucja/KonstytucjaParser.java
@@ -9,34 +9,43 @@ import java.util.Scanner;
 
 public class KonstytucjaParser {
 	
-	public static void main() {
+	public static void main(String[] args) {
 		
 		String filePath;
 		String range;
 		ArrayList <Chapter> chapters = new ArrayList<>();
 		BufferedReader reader = null;
 		
-		System.out.println("Enter file path: ");
+		/*System.out.println("Enter file path: ");
 		Scanner scanner = new Scanner(System.in);
 		filePath = scanner.nextLine();
 		range = scanner.nextLine();
-		scanner.close();
+		scanner.close(); */
+		
+		filePath = args[0];
+		range = args[1];
 		
 		try
 		  {
-		    reader = new BufferedReader(new FileReader(filePath + "konstytucja.txt"));
-		    String line;	
-		    while ((line = reader.readLine()) != null)
-		    {
-		      if(line.startsWith("@")) {
-		    	  reader.readLine();
-		    	  line = reader.readLine();
-		      }
-		      if(line.startsWith("Rozdzia")) {
-		    	  Chapter chapter = new Chapter(chapters.size()+1);
-		    	  chapter.title = line;
-		    	  System.out.println(range);
-		      }
+		    reader = new BufferedReader(new FileReader(filePath + "\\konstytucja.txt"));
+		    String line;
+		    if(reader != null){
+			    while ((line = reader.readLine()) != null)
+			    {
+			      if(line.startsWith("@")) {
+			    	  reader.readLine();
+			    	  line = reader.readLine();
+			      }
+			      if(line.startsWith("Rozdzia")) {
+			    	  Chapter chapter = new Chapter(chapters.size()+1);
+			    	  line = reader.readLine();
+			    	  chapter.title = line;
+			    	  chapters.add(chapter);
+			    	  System.out.println(chapter.decToRoman(chapter.number));
+			    	  chapter.printChapter();
+			    	  reader.readLine();
+			      }
+			    }
 		    }
 		  } catch (FileNotFoundException e) {
 			    e.printStackTrace();
--- src/konstytucja/Point.java
+++ src/konstytucja/Point.java
@@ -14,3 +14,4 @@ public class Point {
 		return (this.text);
 	}
 }
+
